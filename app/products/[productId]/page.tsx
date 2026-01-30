f1e2892b151afa7d58b601794e338e96c5e46b2a
Jephthah Onuoha <geoffreydesignsng@gmail.com>
2026-01-30 18:46:22 +0100

    Changes to Product Details and Cart pages

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -40,6 +40,7 @@ const ProductDetails = () => {
   const [formValues, setFormValues] = useState({
     sizes: [],
     slots: 0,
+    colors: []
   })
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value, checked } = e.target;
@@ -78,64 +79,101 @@ const ProductDetails = () => {
     }));
   };
 
+  const colorsInput = ["red", "white", "black", "yellow"];
+
   return (
     <section className='pt-24 mb-10 md:mb-16'>
       <div className="px-4 md:px-10 lg:px-20 flex flex-col gap-8 items-start">
-        <div className="p-4 rounded-lg bg-(--bg-surface) hidden md:flex flex-col md:flex-row justify-between gap-4 items-center w-full">
-          <div className="flex items-center gap-4">
-            <Image src={supplierInfo.logo} alt="" width={0} height={0} className="w-20 aspect-square rounded-full" />
-            <div>
-              <h2 className="text-xl">{ supplierInfo.name }</h2>
-              <p className="text-(--text-muted)">{ supplierInfo.city }, { supplierInfo.country }</p>
-              {
-                supplierInfo.verifiedStatus &&
-                <Badge primary className="font-semibold">{ supplierInfo.verificationType }</Badge>
-              }
-            </div>
-          </div>
-          <div className="flex gap-8">
-            <div className="flex flex-col">
-              <div className="mb-1">
-                <p className="text-(--text-muted) uppercase text-xs">In Business</p>
-                <p className="font-semibold">12 Years</p>
-              </div>
-              <div>
-                <p className="text-(--text-muted) uppercase text-xs">Response Time</p>
-                <p className="font-semibold">Less than 4 Hours</p>
+        <div className="flex flex-col md:flex-row gap-8 items-start w-full">
+          <div className="w-full md:basis-1/2 lg:basis-4/5">
+            <div className="bg-(--bg-surface) p-4 md:p-6 rounded-xl md:mb-8">
+              <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
+              <div className="grid grid-cols-4 gap-4 mt-4">
+                <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
+                <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
+                <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
+                <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
               </div>
             </div>
-            <div className="flex flex-col">
-              <div className="mb-1">
-                <p className="text-(--text-muted) uppercase text-xs">Rating</p>
-                <p className="font-semibold">{ supplierInfo.rating } / 5.0</p>
-              </div>
-              <div>
-                <p className="text-(--text-muted) uppercase text-xs">Transactions</p>
-                <p className="font-semibold">5k+</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <Button primary className="py-2! rounded-xl!">
-              View Supplier
-            </Button>
-            <Button className="py-2! rounded-xl!">
-              Message Supplier
-            </Button>
-          </div>
-        </div>
+            <div className="hidden md:block p-4 md:p-6 rounded-2xl bg-(--bg-surface) w-full mb-8">
+              <Tabs defaultValue="specs">
+                <Tabs.List className="border-b border-(--border-default)">
+                  <Tabs.Trigger
+                    value="specs"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Specs</span>
+                    <span className="hidden md:block">Specifications</span>
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    value="shipping"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Shipping</span>
+                    <span className="md:block hidden">Shipping Information</span>
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    value="reviews"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Reviews</span>
+                    <span className="md:block hidden">Product Reviews</span>
+                  </Tabs.Trigger>
+                </Tabs.List>
 
-        <div className="p-2 md:p-8 rounded-2xl bg-(--bg-surface) flex flex-col md:flex-row justify-between gap-8 items-start w-full">
-          <div className="w-full md:basis-1/4">
-            <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
-            <div className="grid grid-cols-4 gap-2 mt-3">
-              <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
-              <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
-              <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
-              <Image src={productInfo.image} alt="" width={0} height={0} className="w-full aspect-square rounded-xl" />
+                <Tabs.Content value="specs" className="pt-4">
+                  Product details go here
+                </Tabs.Content>
+                <Tabs.Content value="shipping" className="pt-4">
+                  Shipping details go here
+                </Tabs.Content>
+                <Tabs.Content value="reviews" className="pt-4">
+                  Reviews content goes here
+                </Tabs.Content>
+              </Tabs>
+            </div>
+            <div className="p-4 rounded-lg bg-(--bg-surface) hidden md:flex flex-col md:flex-row justify-between gap-4 items-center w-full">
+              <div className="flex items-center gap-4">
+                <Image src={supplierInfo.logo} alt="" width={0} height={0} className="w-20 aspect-square rounded-full" />
+                <div>
+                  <h2 className="text-xl">{supplierInfo.name}</h2>
+                  <p className="text-(--text-muted)">{supplierInfo.city}, {supplierInfo.country}</p>
+                  {
+                    supplierInfo.verifiedStatus &&
+                    <Badge primary className="font-semibold">{supplierInfo.verificationType}</Badge>
+                  }
+                </div>
+              </div>
+              <div className="flex gap-8">
+                <div className="flex flex-col">
+                  <div className="mb-1">
+                    <p className="text-(--text-muted) uppercase text-xs">In Business</p>
+                    <p className="font-semibold">12 Years</p>
+                  </div>
+                  <div>
+                    <p className="text-(--text-muted) uppercase text-xs">Response Time</p>
+                    <p className="font-semibold">Less than 4 Hours</p>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="mb-1">
+                    <p className="text-(--text-muted) uppercase text-xs">Rating</p>
+                    <p className="font-semibold">{supplierInfo.rating} / 5.0</p>
+                  </div>
+                  <div>
+                    <p className="text-(--text-muted) uppercase text-xs">Transactions</p>
+                    <p className="font-semibold">5k+</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <Button primary className="py-2! rounded-xl!">
+                  View Profile
+                </Button>
+              </div>
             </div>
           </div>
-          <div className="basis-full md:basis-3/4">
+          <div className="w-full md:basis-1/2 lg:basis:1/5 bg-(--bg-surface) p-4 md:p-6 rounded-xl sticky top-0">
             <h1 className="text-3xl lg:text-4xl">{ productInfo.name }</h1>
             <div className="flex gap-2 md:gap-4 items-center flex-wrap mb-4">
               <div className="flex gap-1 items-center">
@@ -167,10 +205,10 @@ const ProductDetails = () => {
                 <p className="text-xl font-bold">$1.50</p>
               </div> 
             </div>
-            <div className="rounded-xl p-4 md:p-6 bg-(--primary-soft) border-2 border-(--primary) mb-4">
+            <div className="rounded-xl p-4 bg-(--primary-soft) border-2 border-(--primary) mb-4">
               <div className="flex md:items-end justify-between">
                 <div className="flex flex-col gap-2">
-                  <div className="flex gap-2 items-center text-(--primary)">
+                  <div className="hidden md:flex gap-2 items-center text-(--primary)">
                     <RiGroupFill />
                     <h2 className="text-xl uppercase"><span className="hidden md:block"></span> Bale Progress</h2>
                   </div>
@@ -180,7 +218,7 @@ const ProductDetails = () => {
                   </p>
                 </div>
                 <div className="flex flex-col items-end gap-2">
-                  <div className="flex gap-2 items-center text-(--text-muted)">
+                  <div className="hidden md:flex gap-2 items-center text-(--text-muted)">
                     <RiTimeFill />
                     <p className="font-bold uppercase">Time Left</p>
                   </div>
@@ -196,54 +234,79 @@ const ProductDetails = () => {
                 progClass="bg-(--bg-page)/70!"
               />
               <Alert type="success" className="mt-3 py-2! px-4! w-fit! text-sm!">
-                * 1 slot contains 20 products
+                * 1 slot = 20 products
               </Alert>
             </div>
-            <div className="mb-4">
-              <p className="text-(--text-muted) uppercase font-semibold">Sizes</p>
-              <Input
-                element="input"
-                input_type="checkbox"
-                name="sizes"
-                value={formValues.sizes}
-                handler={handleCheckboxChange}
-                checkboxOptions={["S", "M", "L", "XL", "XXL"]}
-                genStyle="my-0!"
-              />
+            <div className="mb-4 flex flex-col md:flex-row gap-4">
+              <div>
+                <p className="text-(--text-muted) uppercase font-semibold">Sizes</p>
+                <Input
+                  element="input"
+                  input_type="checkbox"
+                  name="sizes"
+                  value={formValues.sizes}
+                  handler={handleCheckboxChange}
+                  checkboxOptions={["S", "M", "L", "XL", "XXL"]}
+                  genStyle="my-0!"
+                  styling="p-2!"
+                />
+              </div>
+              <div>
+                <p className="text-(--text-muted) uppercase font-semibold">Colors</p>
+                <div className="flex gap-1">
+                  {
+                    colorsInput.map(color => (
+                      <Input
+                        element="input"
+                        input_type="checkbox"
+                        name="colors"
+                        value={formValues.colors}
+                        handler={handleCheckboxChange}
+                        checkboxOptions={[`${color}`]}
+                        genStyle="my-0!"
+                        styling={`p-2! rounded-full! ${color == "black" || color == "white" ? `bg-${color}` : `bg-${color}-500`}`}
+                        invisible
+                      />
+                    ))
+                  }
+                </div>
+              </div>
             </div>
-            <div className="mb-4 flex flex-col md:flex-row md:items-end gap-4">
+            <div className="mb-4 flex flex-col gap-4">
               <div>
                 <p className="text-(--text-muted) uppercase font-semibold">Slots</p>
                 <div className="flex items-stretch">
-                  <Button 
-                    className="rounded-r-none"
+                  <Button
+                    className="rounded-r-none rounded-l-xl! py-2!"
                     disabled={Boolean(formValues.slots == 0)}
-                    onClick={() => 
+                    onClick={() =>
                       setFormValues(prev => ({
                         ...prev,
                         slots: prev.slots == 0 ? 0 : prev.slots - 1
                       }))
                     }
+                    primary
                   >
                     -
                   </Button>
                   <Input
                     element="input"
-                    input_type="number"
+                    input_type="text"
                     name="quantity"
                     value={formValues.slots}
                     handler={handleChange}
                     genStyle="my-0!"
-                    styling="rounded-none"
+                    styling="rounded-none p-2! focus:outline-none! disabled w-30!"
                   />
-                  <Button 
-                    className="rounded-l-none"
+                  <Button
+                    className="rounded-l-none rounded-r-xl! py-2!"
                     onClick={() =>
                       setFormValues(prev => ({
                         ...prev,
                         slots: prev.slots + 1
                       }))
                     }
+                    primary
                   >
                     +
                   </Button>
@@ -252,15 +315,52 @@ const ProductDetails = () => {
               <div>
                 <p className="text-(--text-muted) uppercase font-semibold">Total Estimated Price</p>
                 <p className="uppercase font-bold text-3xl">
-                  $650 
+                  $650
                   <i className="text-(--text-muted) text-xs font-normal">(+ Shipping)</i>
                 </p>
               </div>
             </div>
+            <div className="block md:hidden rounded-2xl bg-(--bg-surface) w-full mb-8">
+              <Tabs defaultValue="specs">
+                <Tabs.List className="border-b border-(--border-default)">
+                  <Tabs.Trigger
+                    value="specs"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Specs</span>
+                    <span className="hidden md:block">Specifications</span>
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    value="shipping"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Shipping</span>
+                    <span className="md:block hidden">Shipping Information</span>
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    value="reviews"
+                    className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
+                  >
+                    <span className="block md:hidden">Reviews</span>
+                    <span className="md:block hidden">Product Reviews</span>
+                  </Tabs.Trigger>
+                </Tabs.List>
+
+                <Tabs.Content value="specs" className="pt-4">
+                  Product details go here
+                </Tabs.Content>
+                <Tabs.Content value="shipping" className="pt-4">
+                  Shipping details go here
+                </Tabs.Content>
+                <Tabs.Content value="reviews" className="pt-4">
+                  Reviews content goes here
+                </Tabs.Content>
+              </Tabs>
+            </div>
             <div className="mb-4 flex gap-4 items-center">
               <Button primary className="uppercase flex gap-2 items-center">
-                <RiRocket2Fill />
-                Join Bale
+                <RiRocket2Fill className="hidden md:block" />
+                Join Pool
               </Button>
               <Button primary className="uppercase ring-2 ring-(--primary) ring-inset text-(--primary)! bg-transparent">
                 Add to Cart
@@ -277,44 +377,6 @@ const ProductDetails = () => {
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="p-4 md:p-8 rounded-2xl bg-(--bg-surface) w-full">
-          <Tabs defaultValue="specs">
-            <Tabs.List className="border-b border-(--border-default)">
-              <Tabs.Trigger
-                value="specs"
-                className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
-              >
-                <span className="block md:hidden">Specs</span>
-                <span className="hidden md:block">Specifications</span>
-              </Tabs.Trigger>
-              <Tabs.Trigger
-                value="shipping"
-                className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
-              >
-                <span className="block md:hidden">Shipping</span>
-                <span className="md:block hidden">Shipping Information</span>
-              </Tabs.Trigger>
-              <Tabs.Trigger
-                value="reviews"
-                className="px-4 py-2 data-[state=active]:border-b-3 data-[state=active]:border-(--primary) data-[state=active]:text-(--primary)"
-              >
-                <span className="block md:hidden">Reviews</span>
-                <span className="md:block hidden">Product Reviews</span>
-              </Tabs.Trigger>
-            </Tabs.List>
-
-            <Tabs.Content value="specs" className="pt-4">
-              Product details go here
-            </Tabs.Content>
-            <Tabs.Content value="shipping" className="pt-4">
-              Shipping details go here
-            </Tabs.Content>
-            <Tabs.Content value="reviews" className="pt-4">
-              Reviews content goes here
-            </Tabs.Content>
-          </Tabs>
         </div>
       </div>
     </section>
