dd78e2b154d2bf787a352ae9a18c917d870ce1a7
Jephthah Onuoha <geoffreydesignsng@gmail.com>
2026-02-06 21:24:50 +0100

    Made minor changes to ImageGallery

--- components/product/ImageGallery.tsx
+++ components/product/ImageGallery.tsx
@@ -11,12 +11,14 @@ export default function ProductImages({ imageList, countdown }: { imageList: str
     <>
       <div className="rounded-xl md:mb-8 bg-(--bg-surface) flex flex-col-reverse md:flex-row gap-4 items-stretch w-full">
         {/* Thumbnails */}
-        <div className="
-          flex gap-2 w-[70vw] md:w-20 md:gap-4
-          md:flex-col md:basis-1/6
-          md:overflow-y-auto md:overflow-x-hidden md:h-120
-          no-scrollbar overflow-x-auto overflow-y-hidden
-        ">
+        <div 
+          className="
+            flex gap-2 w-[70vw] md:w-20 md:gap-4
+            md:flex-col md:basis-1/6
+            md:overflow-y-auto md:overflow-x-hidden md:h-120
+            no-scrollbar overflow-x-auto overflow-y-hidden
+          "
+        >
           {imageList.map((image, index) => (
             <div
               key={index}
@@ -34,15 +36,17 @@ export default function ProductImages({ imageList, countdown }: { imageList: str
         </div>
 
         {/* Main image */}
-        <div className="w-full! md:basis-5/6 relative">
-          <img
-            src={mainImage}
-            alt="Product image"
-            width={0}
-            height={0}
-            className="w-full h-120 rounded-xl object-contain"
-          />
-          { countdown }
+        <div className="w-full! md:basis-5/6">
+          <div className="relative w-fit mx-auto">
+            <img
+              src={mainImage}
+              alt="Product image"
+              width={0}
+              height={0}
+              className="w-full h-120 rounded-xl object-contain"
+            />
+            {countdown}
+          </div>
         </div>
       </div>
 
